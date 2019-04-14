9df9535f7000446a8afb9794bbe5df2d66f8980f
MatthewCookUNR <matthewthomascook@yahoo.com>
2019-04-14 13:12:06 -0700

    Added ease of use and error improvements

--- SecretMessage/WindowBuilderGUI/src/SecretMessagesGUI.java
+++ SecretMessage/WindowBuilderGUI/src/SecretMessagesGUI.java
@@ -12,6 +12,9 @@ import java.awt.Color;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.JScrollPane;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 //Program creates a GUI that uses classic Caesar cipher to encode and decode messages
 public class SecretMessagesGUI extends JFrame 
@@ -20,7 +23,10 @@ public class SecretMessagesGUI extends JFrame
 	private JTextArea textIn;
 	private JTextArea textOut;
 	private JSlider slider;
+	private JScrollPane scrollPane;
+	private JScrollPane scrollPane_1;
 	
+	//Constructs GUI
 	public SecretMessagesGUI() 
 	{
 		//Initializes JFrame
@@ -29,24 +35,55 @@ public class SecretMessagesGUI extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
+		//Allows textIn area to scroll for long messages
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 568, 140);
+		getContentPane().add(scrollPane);
+		
 		//Text area for user to write message that will be encoded/decoded
 		textIn = new JTextArea();
+		scrollPane.setViewportView(textIn);
 		textIn.setWrapStyleWord(true);
 		textIn.setLineWrap(true);
 		textIn.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		textIn.setBounds(10, 11, 568, 140);
-		getContentPane().add(textIn);
+		
+		//Allows textOut area to scroll for long messages
+		scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 214, 568, 140);
+		getContentPane().add(scrollPane_1);
 		
 		//Text area that presents the encoded/decoded message
 		textOut = new JTextArea();
+		scrollPane_1.setViewportView(textOut);
 		textOut.setWrapStyleWord(true);
 		textOut.setLineWrap(true);
 		textOut.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		textOut.setBounds(10, 214, 568, 140);
-		getContentPane().add(textOut);
 		
 		//Text field that allows user to input the key for the cipher
 		textKey = new JTextField();
+		textKey.addKeyListener(new KeyAdapter() //Code updates slider
+		{
+			@Override
+			public void keyReleased(KeyEvent e) 
+			{
+				try
+				{
+					int intKey = Integer.parseInt(textKey.getText());
+					if(intKey < -25 || intKey > 25)
+					{
+						throw new ArithmeticException("Out of desired bounds");
+					}
+					slider.setValue(intKey);
+				}
+				catch(Exception ex)
+				{
+					JOptionPane.showMessageDialog(getContentPane(), 
+							"Please enter a whole number between -25 and 25 for the encryption key.");
+					textKey.requestFocus();
+					textKey.selectAll();
+				}
+			}
+		});
 		textKey.setText("0");
 		textKey.setHorizontalAlignment(SwingConstants.CENTER);
 		textKey.setBounds(259, 172, 49, 20);
@@ -68,6 +105,10 @@ public class SecretMessagesGUI extends JFrame
 				{
 					String myMessage = textIn.getText();
 					int myKey = Integer.parseInt(textKey.getText());
+					if(myKey < -25 || myKey > 25)
+					{
+						throw new ArithmeticException("Out of desired bounds");
+					}
 					String output = encodeDecode(myMessage, myKey);
 					textOut.setText(output);
 				}
@@ -84,6 +125,23 @@ public class SecretMessagesGUI extends JFrame
 		btnEncodedecode.setBounds(318, 171, 130, 23);
 		getContentPane().add(btnEncodedecode);
 		
+		//Button moves output text to input text
+		JButton btnMoveUp = new JButton("Move Up ^");
+		btnMoveUp.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e)
+			{
+				String outText = textOut.getText();
+				textIn.setText(outText);
+				int slidValue = slider.getValue() * -1;
+				slider.setValue(slidValue);
+			}
+		});
+		btnMoveUp.setBounds(460, 171, 89, 23);
+		getContentPane().add(btnMoveUp);
+		
+		
+		//Adds slider to provide ease of use and key range
+		//Encodes/decodes as number changes
 		slider = new JSlider();
 		slider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) 
@@ -115,7 +173,8 @@ public class SecretMessagesGUI extends JFrame
 		String output = "";
 		String rvsedMessage = "";
 		char key = (char) keyVal;
-		//Reverses the message
+		
+		//Reverses the message to make it harder to guess
 		for(int i = message.length()-1; i >= 0; i--)
 		{
 			rvsedMessage += message.charAt(i);
